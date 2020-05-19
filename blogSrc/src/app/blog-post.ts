c67b3851ceaa5768d837863efb6381531f93d26a
ejw3md <ejw3md@mail.missouri.edu>
2020-05-19 00:44:30 -0500

    Added blog post examples

---
+++ blogSrc/src/app/blog-post.ts
@@ -0,0 +1,7 @@
+export interface BlogPost {
+    title: string;          /* title of post                */
+    date: string;           /* date of post                 */
+    contentPath: string;    /* content filepath on server   */
+    photoPath: string;      /* photo filepath on server     */
+    primaryContent: string; /* content shown on main page   */
+}
--- blogSrc/src/app/blog-post/blog-post.component.ts
+++ blogSrc/src/app/blog-post/blog-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogPost } from '../blog-post'
 
 @Component({
   selector: 'app-blog-post',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogPostComponent implements OnInit {
   
+  blogPost : BlogPost = {
+    title: "test",
+    date: "5/19/2020",
+    contentPath: "",
+    photoPath: "assets/img/logo_temp.png",
+    primaryContent: "This is a test post in order to display the functionaility of the blogpost module and I wanted to see how long I could make this string in order to asldfjalk;jsdf",
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
